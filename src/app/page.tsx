8901bee88a5100fadfd823a872d912c4a64b6b13
Leart Kaceli <leart.kaceli@gmail.com>
2026-07-11 14:12:11 -0400

    Create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
-  return(
-    <main>
-      <h1>AP Path Planner</h1>
-      <p>
-        Your personalized AP course managment and study planning platform
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        AP Path Planner
+      </h1>
+
+      <p className="mt-4 text-xl text-gray-600">
+        Plan smarter. Study better. Achieve your AP goals.
       </p>
     </main>
   );
